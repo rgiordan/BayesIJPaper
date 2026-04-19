90f4fc50fa4fb4beec7473e29db7a4bdbb1e794d
Ryan Giordano <rgiordan@gmail.com>
2026-04-18 18:04:06 -0700

    remove old postprocessing scripts

--- src/singular_simulations/sync_remote_files.sh
+++ src/singular_simulations/sync_remote_files.sh
@@ -3,7 +3,7 @@
 # Copy selected bootstraps to the local directory
 
 LOCAL_BASE_DIR=$(git rev-parse --show-toplevel)
-SCF_BASE_DIR=/accounts/fac/rgiordano/Documents/git_repos/BayesIJPaper
+REMOTE_BASE_DIR=SET_REMOTE_DIR
 OUTPUT_DIR="src/singular_simulations/output"
 HOST=scf3
 
@@ -11,7 +11,7 @@ FILES=super_simple_simulation_sim*_results_redim100_obsperre100_seed100.Rdata
 
 rsync -vai \
     $HOST:$SCF_BASE_DIR/$MODEL_DIR/${OUTPUT_DIR}/${FILES} \
-    $LOCAL_BASE_DIR/$MODEL_DIR/${OUTPUT_DIR}/
+    $REMOTE_BASE_DIR/$MODEL_DIR/${OUTPUT_DIR}/
 
 
 
