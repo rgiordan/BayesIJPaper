be8e4de66341ad1647425c82bb6a61cd39a5eead
Ryan Giordano <rgiordan@gmail.com>
2026-04-20 14:17:12 -0700

    create zipfile update

--- paper/create_zipfile.sh
+++ paper/create_zipfile.sh
@@ -2,5 +2,10 @@
 
 GIT_REPO=$(git rev-parse --show-toplevel)
 
-zip -r -sf ${GIT_REPO}/paper/bayesij_code.zip ${GIT_REPO} --exclude ${GIT_REPO}"/src/rstanarm/cluster/output/ARM_*.Rdata" ".git/*"
+# Zip the repository, including data files, but excluding large
+# collections of large intermediate data files.
+zip -r -sf ${GIT_REPO}/paper/bayesij_code.zip ${GIT_REPO} \
+    --exclude ".git/*" \
+    --exclude ${GIT_REPO}"/src/rstanarm/cluster/output/ARM_*.Rdata" \
+    --exclude ${GIT_REPO}"/src/singular_simulations/output/super_simple_simulation_sim?*_results_redim100_obsperre100_seed100.Rdata"
 
